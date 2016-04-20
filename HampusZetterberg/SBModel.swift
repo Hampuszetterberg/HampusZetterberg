ce7adcaf2172aedf76f867f89d0085996d368a7d
Hampus Zätterberg <hampus.zatterberg@mobenga.com>
2016-04-20 17:52:42 +0200

    Created Systembolaget Model

---
+++ HampusZetterberg/SBModel.swift
@@ -0,0 +1,20 @@
+//
+//  SBModel.swift
+//  HampusZetterberg
+//
+//  Created by Hampus Zätterberg on 2016-04-20.
+//  Copyright © 2016 hampus. All rights reserved.
+//
+
+import Foundation
+
+class SBModel {
+    var name: String = ""
+    var type: String = ""
+    var year: Int = 0
+    var volume: Int = 0
+    var price: Int = 0
+    var alcoholPercent: Int = 0
+    var ecological: Bool = false
+    
+}
