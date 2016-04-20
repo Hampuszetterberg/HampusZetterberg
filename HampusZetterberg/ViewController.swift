037fa27dce686d2ec1677de1a8db31c75e05b6d6
Hampus Zätterberg <hampus.zatterberg@mobenga.com>
2016-04-20 17:01:37 +0200

    Initial Commit

---
+++ HampusZetterberg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HampusZetterberg
+//
+//  Created by Hampus Zätterberg on 2016-04-20.
+//  Copyright © 2016 hampus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
